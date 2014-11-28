34d8b6cfb5a0c32defba0501329d2392986ef078
ahwalker <ahwalker@uw.edu>
2014-11-28 10:30:18 -0800

    complete integration test

--- test/integration/com/grailsinaction/UserIntegrationIntegrationSpec.groovy
+++ test/integration/com/grailsinaction/UserIntegrationIntegrationSpec.groovy
@@ -4,12 +4,15 @@ import spock.lang.*
 
 class UserIntegrationIntegrationSpec extends Specification {
 
-    def setup() {
-    }
-
-    def cleanup() {
-    }
-
-    void "test something"() {
-    }
+    def "saving a user to the DB"() {
+		given: "A brand spanking new user"
+		def joe = new User(loginId: 'joe', password: 'secret',
+			homepage: 'http://www.joe.com')
+		when: "the user is saved"
+		joe.save()
+		then: "it saved successfully and can be found in the DB"
+		joe.errors.errorCount == 0
+		joe.id != null
+		User.get(joe.id).loginId == joe.loginId
+	}
 }
