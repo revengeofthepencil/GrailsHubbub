8e61fde54d3e72e61d4015cc971d78a18fd5db73
ahwalker <ahwalker@uw.edu>
2014-11-28 10:21:09 -0800

    update gitignore for eclipse

--- test/integration/com/grailsinaction/UserIntegrationIntegrationSpec.groovy
+++ test/integration/com/grailsinaction/UserIntegrationIntegrationSpec.groovy
@@ -1,8 +1,8 @@
 package com.grailsinaction
 
-import grails.test.spock.IntegrationSpec
+import spock.lang.*
 
-class UserIntegrationIntegrationSpec extends IntegrationSpec {
+class UserIntegrationIntegrationSpec extends Specification {
 
     def setup() {
     }
