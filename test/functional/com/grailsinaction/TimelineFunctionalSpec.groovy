ac0e10da4ba8a96f4dc7716b55eaf0340febcbaf
Alex Walker <alexwalker@Alexs-MacBook-Pro.local>
2014-12-09 17:57:41 -0800

    add function Page objects

--- test/functional/com/grailsinaction/TimelineFunctionalSpec.groovy
+++ test/functional/com/grailsinaction/TimelineFunctionalSpec.groovy
@@ -1,38 +1,40 @@
 package com.grailsinaction
 
+import com.grailsinaction.pages.TimelinePage;
+
 import geb.spock.GebReportingSpec
 import spock.lang.Stepwise
 
 class TimelineFunctionalSpec extends GebReportingSpec {
 	def "Does timeline load for user 'phil'"() {
 		when:
-		go "users/phil"
+		to TimelinePage, "phil"
 
 		then:
-		$("#newPost h3").text() == "What is Phil Potts hacking on right now?"
+		whatHeading.text() ==  "What is Phil Potts hacking on right now?"
 	}
 
 	def "Submitting a new Post"() {
 		given: "A user logs in and starts at timeline page"
 		login "frankie", "testing"
-		go "users/phil"
+		to TimelinePage, "phil"
 		
 		when: "A new post is entered"
-		$("#postContent").value("this is the new post")
-		$("#newPost").find("input", type: "button").click()
-		
+		newPostContent.value("this is the new post") 
+		submitPostButton.click()
+
 		then: "the new post is shown on the timeline"
 		waitFor{
-			$("div.postText", text: "this is the new post")
+			!posts("this is the new post").empty
 		}
 		
 	}
 	
 	
 	private login(String username, String password) {
-		go "login/form"
-		$("input[name='loginId']").value(username)
-		$("input[name='password']").value(password)
-		$("input[type='submit']").click()
+        to LoginPage
+        loginIdField = username
+        passwordField = password
+        signInButton.click()
 	}
 }
